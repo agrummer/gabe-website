30e5cd969006fa3dea080644aae0e45bcf71f180
Gabe Cohn <gabecohn@gmail.com>
2015-03-01 14:06:38 -0800

    Added URL field to press JSON

--- js/gabecohn.js
+++ js/gabecohn.js
@@ -1249,6 +1249,7 @@ var awardsJSON_new = [
 /* Press JSON structure
     Each press object contains the following:
         title - string, title of the press article to display
+        url - string, URL to press article. If NULL, then there should be no link
         author - string, author of the press article to display
         publication - string, publication of the press article to display
         sortDate - string in the form YYYY-MM-DD. Used to sort awards by date
@@ -1260,6 +1261,7 @@ var awardsJSON_new = [
 var pressJSON_new = [
     {
         "title": "Bright Idea: SNUPI Technologies & Wally",
+        "url": "http://seattlebusinessmag.com/article/bright-idea-snupi-technologies-wally",
         "author": "John Levesque",
         "publication": "Seattle Business Magazine",
         "sortDate": "2014-03-00",
