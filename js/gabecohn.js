b008ecee9a074e3b5ddea8df4d7e06dd4987e6c6
Gabe Cohn <gabecohn@gmail.com>
2015-03-01 15:09:09 -0800

    completed publications JSON and awards JSON

--- js/gabecohn.js
+++ js/gabecohn.js
@@ -1154,7 +1154,7 @@ var publicationsJSON_new = [
         "id": "pub_C9",
         "index": "C.9",
         "type": "conference",
-        "link": "df/Chen_uTouch_chi13.pdf",
+        "link": "pdf/Chen_uTouch_chi13.pdf",
         "year": "2013",
         "authors": "Chen, K., <div class=\"authorHighlight\">Cohn, G.</div>, Gupta, S., Patel, S.N.",
         "title": "uTouch: Sensing Touch Gestures on Unmodified LCDs",
@@ -1162,27 +1162,447 @@ var publicationsJSON_new = [
         "acceptance": "20% (392/1963)",
         "links": [
             {
-                "title": "paper",
+                "title": "Paper",
                 "type": "doc",
-                "url": "df/Chen_uTouch_chi13.pdf"
+                "url": "pdf/Chen_uTouch_chi13.pdf"
             },
             {
-                "title": "video",
+                "title": "Video",
                 "type": "video",
                 "url": "http://youtu.be/8p9DkwWVmfI"
             },
             {
-                "title": "ACM Digital Library",
+                "title": "Digital Library",
                 "type": "dl",
                 "url": "http://dl.acm.org/citation.cfm?id=2481356"
             },
             {
-                "title": "project info",
+                "title": "Project Info",
                 "type": "proj",
                 "url": "#proj_utouch"
             }
         ],
         "relatedProjects": [ "proj_utouch" ]
+    },
+    {
+        "id": "pub_C8",
+        "index": "C.8",
+        "type": "conference",
+        "link": "pdf/Cohn_SEFS_ubicomp12.pdf",
+        "year": "2012",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Gupta, S., Lee, T., Morris, D., Smith, J.R., Reynolds, M.S., Tan, D.S., Patel, S.N.",
+        "title": "An Ultra-Low-Power Human Body Motion Sensor Using Static Electric Field Sensing",
+        "publication": "In the <em>Proceedings of Ubicomp 2012</em> (Sept. 5-8, Pittsburgh, PA), ACM, New York, 2012, pp. 99-102.",
+        "acceptance": "19% (58/301)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_SEFS_ubicomp12.pdf"
+            },
+            {
+                "title": "Video",
+                "type": "video",
+                "url": "http://youtu.be/Q4EzGqmDEJ8"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://dl.acm.org/citation.cfm?id=2370216.2370233"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_sefs"
+            }
+        ],
+        "relatedProjects": [ "proj_sefs" ]
+    },
+    {
+        "id": "pub_C7",
+        "index": "C.7",
+        "type": "conference",
+        "link": "pdf/Cohn_Humantenna_chi12.pdf",
+        "year": "2012",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Morris, D., Patel, S.N., Tan, D.S.",
+        "title": "Humantenna: Using the Body as an Antenna for Real-Time Whole-Body Interaction",
+        "publication": "In the <em>Proceedings of CHI 2012</em> (May 5-10, Austin, TX), ACM, New York, 2012, pp. 1901-1910.",
+        "acceptance": "23% (370/1577)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_Humantenna_chi12.pdf"
+            },
+            {
+                "title": "Video",
+                "type": "video",
+                "url": "http://www.youtube.com/watch?v=hfAk1Vnj6hM"
+            },
+            {
+                "title": "Talk Video",
+                "type": "video",
+                "url": "video/Cohn_Humantenna_chi12_talk.mp4"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://dl.acm.org/citation.cfm?id=2207676.2208330"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_humantenna"
+            }
+        ],
+        "relatedProjects": [ "proj_humantenna" ]
+    },
+    {
+        "id": "pub_C6",
+        "index": "C.6",
+        "type": "conference",
+        "link": "pdf/Cohn_NoiseCommand_chi11.pdf",
+        "year": "2011",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Morris, D., Patel, S.N., Tan, D.S.",
+        "title": "Your Noise is My Command: Sensing Gestures Using the Body as an Antenna",
+        "publication": "In the <em>Proceedings of CHI 2011</em> (May 7-12, Vancouver, Canada), ACM, New York, 2011, pp. 791-800.",
+        "acceptance": "26% (400/1540)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_NoiseCommand_chi11.pdf"
+            },
+            {
+                "title": "Talk Video",
+                "type": "video",
+                "url": "video/Cohn_NoiseCommand_chi11_talk.mp4"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://portal.acm.org/citation.cfm?id=1979058"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_humantenna"
+            }
+        ],
+        "relatedProjects": [ "proj_humantenna" ]
+    },
+    {
+        "id": "pub_C5",
+        "index": "C.5",
+        "type": "conference",
+        "link": "pdf/Badshah_InGen_chi11.pdf",
+        "year": "2011",
+        "authors": "Badshah, A., Gupta, S., <div class=\"authorHighlight\">Cohn, G.</div>, Villar, N., Hodges, S., Patel, S.N.",
+        "title": "Interactive Generator: A Self-Powered Haptic Feedback Device",
+        "publication": "In the <em>Proceedings of CHI 2011</em> (May 7-12, Vancouver, Canada), ACM, New York, 2011, pp. 2051-2054.",
+        "acceptance": "26% (400/1540)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Badshah_InGen_chi11.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://portal.acm.org/citation.cfm?id=1979240"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_ingen"
+            }
+        ],
+        "relatedProjects": [ "proj_ingen" ]
+    },
+    {
+        "id": "pub_C4",
+        "index": "C.4",
+        "type": "conference",
+        "link": "pdf/Larson_HeatWave_chi11.pdf",
+        "year": "2011",
+        "authors": "Larson, E., <div class=\"authorHighlight\">Cohn, G.</div>, Gupta, S., Ren, X., Harrison, B., Fox, D., Patel, S.N.",
+        "title": "HeatWave: Thermal Imaging for Surface User Interaction",
+        "publication": "In the <em>Proceedings of CHI 2011</em> (May 7-12, Vancouver, Canada), ACM, New York, 2011, pp. 2565-2574.",
+        "acceptance": "26% (400/1540)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Larson_HeatWave_chi11.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://portal.acm.org/citation.cfm?id=1979317"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_heatwave"
+            }
+        ],
+        "relatedProjects": [ "proj_heatwave" ]
+    },
+    {
+        "id": "pub_C3",
+        "index": "C.3",
+        "type": "conference",
+        "link": "pdf/Campbell_WATTR_ubicomp10.pdf",
+        "year": "2010",
+        "authors": "Campbell, T., Larson, E., <div class=\"authorHighlight\">Cohn, G.</div>, Froehlich, J., ALcaide, R., Patel, S.N.",
+        "title": "WATTR: A Method for Self-Powered Wireless Sensing of Water Activity in the Home",
+        "publication": "In the <em>Proceedings of UbiComp 2010</em> (Sept. 26-29, Copenhagen, Denmark), ACM, New York, 2010, pp. 169-172.",
+        "acceptance": "19% (39/202)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Campbell_WATTR_ubicomp10.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://portal.acm.org/citation.cfm?id=1864378"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_wattr"
+            }
+        ],
+        "relatedProjects": [ "proj_wattr" ]
+    },
+    {
+        "id": "pub_C2",
+        "index": "C.2",
+        "type": "conference",
+        "link": "pdf/Cohn_SNUPI_ubicomp10.pdf",
+        "year": "2010",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Stuntebeck, E., Pandey, J., Otis, B., Abowd, G.D., Patel, S.N.",
+        "title": "SNUPI: Sensor Nodes Utilizing Powerline Infrastructure",
+        "publication": "In the <em>Proceedings of UbiComp 2010</em> (Sept. 26-29, Copenhagen, Denmark), ACM, New York, 2010, pp. 159-168.",
+        "acceptance": "19% (39/202)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_SNUPI_ubicomp10.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://portal.acm.org/citation.cfm?id=1864349.1864377"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_snupi"
+            }
+        ],
+        "relatedProjects": [ "proj_snupi" ]
+    },
+    {
+        "id": "pub_C1",
+        "index": "C.1",
+        "type": "conference",
+        "link": "pdf/Cohn_GasSense_pervasive10.pdf",
+        "year": "2010",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Gupta, S., Froehlich, J., Larson, E., and Patel, S.N.",
+        "title": "GasSense: Appliance-Level, Single-Point Sensing of Gas Activity in the Home",
+        "publication": "In the <em>Proceedings of Pervasive 2010</em> (May 17-20, Helsinki, Finland), Springer-Verlag, Heidelberg, 2010, pp. 265-282.",
+        "acceptance": "16% (26/161)",
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_GasSense_pervasive10.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://link.springer.com/chapter/10.1007/978-3-642-12654-3_16"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_gassense"
+            }
+        ],
+        "relatedProjects": [ "proj_gassense" ]
+    },
+    {
+        "id": "pub_J1",
+        "index": "J.1",
+        "type": "journal",
+        "link": "pdf/Froehlich_DisagEnergy_IEEEpervasive11.pdf",
+        "year": "2011",
+        "authors": "Froehlich, J., Larson, E., Gupta, S., <div class=\"authorHighlight\">Cohn, G.</div>, Reynolds, M.S., Patel, S.N.",
+        "title": "Disaggregated End-Use Energy for the Smart Grid",
+        "publication": "<em>IEEE Pervasive Computing, Special Issue on Smart Energy Systems</em>, 10(1), Jan-Mar 2011, pp. 28-39.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Froehlich_DisagEnergy_IEEEpervasive11.pdf"
+            },
+            {
+                "title": "Digital Library",
+                "type": "dl",
+                "url": "http://www.computer.org/portal/web/csdl/doi/10.1109/MPRV.2010.74"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_gassense"
+            }
+        ],
+        "relatedProjects": [ "proj_gassense" ]
+    },
+    {
+        "id": "pub_W1",
+        "index": "W.1",
+        "type": "workshop",
+        "link": "pdf/Levin_ElectionFraud_EVTWOTE09.pdf",
+        "year": "2009",
+        "authors": "Levin, I., <div class=\"authorHighlight\">Cohn, G.A.</div>, Ordeshook, P.C., Alvarez, R.M.",
+        "title": "Detecting Voter Fraud in an Electronic Voting Context: An Analysis of the Unlimited Reelection Vote in Venezuela",
+        "publication": "In the <em>Proceedings of 2009 Electronic Voting Technology Workshop/Workshop on Trustworthy Elections (EVT/WOTE '09)</em> (Aug. 10-11, Montreal, Canada), USENIX, 2009.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Levin_ElectionFraud_EVTWOTE09.pdf"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_electionfraud"
+            }
+        ],
+        "relatedProjects": [ "proj_electionfraud" ]
+    },
+    {
+        "id": "pub_D1",
+        "index": "D.1",
+        "type": "thesis",
+        "link": null,
+        "year": "2014",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.A.</div>",
+        "title": "SNUPI: Sensor Network Utilizing Powerline Infrastructure",
+        "publication": "<em>University of Washington Doctoral Dissertation</em>, 2014.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_snupi"
+            }
+        ],
+        "relatedProjects": [ "proj_snupi" ]
+    },
+    {
+        "id": "pub_O1",
+        "index": "O.1",
+        "type": "other",
+        "link": "pdf/Cohn_Humantenna_EEK12.pdf",
+        "year": "2012",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.</div>, Morris, D., Patel, S.N., Tan, D.S.",
+        "title": "Humantenna: Using the Body as an Antenna for Real-Time Whole- Body Interaction",
+        "publication": "<em>University of Washington Electrical Engineering Kaleidoscope (EEK) Magazine</em>, 2012, p. 7.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_Humantenna_EEK12.pdf"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_humantenna"
+            }
+        ],
+        "relatedProjects": [ "proj_humantenna" ]
+    },
+    {
+        "id": "pub_R3",
+        "index": "R.3",
+        "type": "report",
+        "link": "pdf/Cohn_CADFeeds_07.pdf",
+        "year": "2007",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.A.</div>",
+        "title": "Computer Modeling of Wideband Tapered-Slot Microwave Antenna Feeds",
+        "publication": "<em>Caltech RF and Microwave Group</em>, 2007.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_CADFeeds_07.pdf"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_antennacad"
+            }
+        ],
+        "relatedProjects": [ "proj_antennacad" ]
+    },
+    {
+        "id": "pub_R2",
+        "index": "R.2",
+        "type": "report",
+        "link": "pdf/Cohn_MeteorInterferometry_06.pdf",
+        "year": "2006",
+        "authors": "<div class=\"authorHighlight\">Cohn, G.A.</div>, Sahr, J.D.",
+        "title": "Meteor radar interferometry using NEC antenna array simulations",
+        "publication": "<em>University of Washington Radar Remote Sensing Laboratory</em>, 2006.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Cohn_MeteorInterferometry_06.pdf"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_rrsl06"
+            }
+        ],
+        "relatedProjects": [ "proj_rrsl06" ]
+    },
+    {
+        "id": "pub_R1",
+        "index": "R.1",
+        "type": "report",
+        "link": "pdf/Lind_ISISFirstLight_06.pdf",
+        "year": "2006",
+        "authors": "Lind F., Berkowitz, Z., Morabito, A., Vertatschitsch, L., <div class=\"authorHighlight\">Cohn, G.</div>, Nguyen, K., Sahr, J.",
+        "title": "RRSL Milestone: First E Region Irregularities on ISIS",
+        "publication": "<em>University of Washington Radar Remote Sensing Laboratory</em>, 2006.",
+        "acceptance": null,
+        "links": [
+            {
+                "title": "Paper",
+                "type": "doc",
+                "url": "pdf/Lind_ISISFirstLight_06.pdf"
+            },
+            {
+                "title": "Project Info",
+                "type": "proj",
+                "url": "#proj_rrsl06"
+            }
+        ],
+        "relatedProjects": [ "proj_rrsl06" ]
     }
 ];
 
@@ -1242,6 +1662,51 @@ var awardsJSON_new = [
         "displayDate": "Sept. 2012",
         "relatedProjects": [ "proj_sefs" ],
         "relatedPublications": [ "pub_C8" ],
+        "relatedTalks": [ ]
+    },
+    {
+        "shortName": "Honorable Mention Award",
+        "longName": "Honorable Mention Award at CHI 2012",
+        "sortDate": "2012-05-05",
+        "displayDate": "May 2012",
+        "relatedProjects": [ "proj_humantenna" ],
+        "relatedPublications": [ "pub_C7" ],
+        "relatedTalks": [ ]
+    },
+    {
+        "shortName": "Best Paper Award",
+        "longName": "Best Paper Award at CHI 2011",
+        "sortDate": "2011-05-07",
+        "displayDate": "May 2011",
+        "relatedProjects": [ "proj_humantenna" ],
+        "relatedPublications": [ "pub_C6" ],
+        "relatedTalks": [ ]
+    },
+    {
+        "shortName": "Best Note Award",
+        "longName": "Best Note Award at CHI 2011",
+        "sortDate": "2011-05-07",
+        "displayDate": "May 2011",
+        "relatedProjects": [ "proj_ingen" ],
+        "relatedPublications": [ "pub_C5" ],
+        "relatedTalks": [ ]
+    },
+    {
+        "shortName": "Honorable Mention Award",
+        "longName": "Honorable Mention Award at CHI 2011",
+        "sortDate": "2011-05-07",
+        "displayDate": "May 2011",
+        "relatedProjects": [ "proj_heatwave" ],
+        "relatedPublications": [ "pub_C4" ],
+        "relatedTalks": [ ]
+    },
+    {
+        "shortName": "Best Paper Nominee",
+        "longName": "Best Paper Nominee at Ubicomp 2010",
+        "sortDate": "2010-09-26",
+        "displayDate": "Sept. 2010",
+        "relatedProjects": [ "proj_snupi" ],
+        "relatedPublications": [ "pub_C2" ],
         "relatedTalks": [ ]
     }
 ];
