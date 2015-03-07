e16a67ac50cfb2999394641e4357d47f303cf96f
Alex Grummer <alex@agrummer.com>
2015-03-07 12:06:56 -0800

    Publications section is now grouped by type with groups sorted by type.sortOrder from a newly added json struct in publications.json

--- js/gabecohn.js
+++ js/gabecohn.js
@@ -28,7 +28,7 @@ $(function () {
 
     renderProjects(projectsJSON);
 
-    renderPublications(publicationsJSON);
+    renderPublications(publicationTypesJSON, publicationsJSON);
 
     renderTalks(talksJSON);
 
@@ -139,26 +139,36 @@ var handleAnchor = function(anchor) {
 
 var addIconsToLinks = function(links) {
     if (links && links.length && links.length > 0) {
-        for (var l = 0; l < links.length; l += 1) {
-            if (links[l].type) {
-                switch(links[l].type) {
-                    case "dl": links[l].icon = "book"; break;
-                    case "doc": links[l].icon = "file"; break;
-                    case "link": links[l].icon = "link"; break;
-                    case "proj": links[l].icon = "tag"; break;
-                    case "pub": links[l].icon = "file"; break;
-                    case "slides": links[l].icon = "file"; break;
-                    case "star": links[l].icon = "star-empty"; break;
-                    case "talk": links[l].icon = "film"; break;
-                    case "video": links[l].icon = "film"; break;
-                    case "web": links[l].icon = "globe"; break;
-                    default: links[l].icon = "file";
+        for (var i = 0; i < links.length; i += 1) {
+            if (links[i].type) {
+                switch(links[i].type) {
+                    case "dl": links[i].icon = "book"; break;
+                    case "doc": links[i].icon = "file"; break;
+                    case "link": links[i].icon = "link"; break;
+                    case "proj": links[i].icon = "tag"; break;
+                    case "pub": links[i].icon = "file"; break;
+                    case "slides": links[i].icon = "file"; break;
+                    case "star": links[i].icon = "star-empty"; break;
+                    case "talk": links[i].icon = "film"; break;
+                    case "video": links[i].icon = "film"; break;
+                    case "web": links[i].icon = "globe"; break;
+                    default: links[i].icon = "file";
                 }
             }
         }
     }
     return links;
 }
+
+// Helper method to find an element within an array which has an attribute of a specified name and value
+var getArrayElementWithAttribute = function(array, nameValue) {
+    for (var i = 0; i < array.length; i += 1) {
+        if (array[i] && array[i][nameValue.name] && array[i][nameValue.name] === nameValue.value) {
+            return array[i];
+        }
+    }
+    return undefined;
+};
 
 var renderProjects = function(jsonData) {
     var $parent = $(".projects-parent"),
@@ -307,20 +317,30 @@ var renderProjects = function(jsonData) {
     $parent.append(renderedHTML);
 };
 
-var renderPublications = function(jsonData) {
+var renderPublications = function(typesJsonData, jsonData) {
     var $parent = $(".publications-parent"),
         htmlTemplate,
+        htmlNavTemplate,
         htmlGroupStartTemplate,
         htmlGroupEndTemplate,
         publication,
+        publicationType,
         publicationGroup,
-        groupId,
+        publicationGroups = [],
         publicationId,
         renderedHTML;
 
+    htmlNavTemplate  = '<div class="subnav well">';
+    htmlNavTemplate += '    <ul class="subnav">';
+    htmlNavTemplate += '        {{#types}}';
+    htmlNavTemplate += '            <li><a href="#publications-{{ name }}">{{ displayName }}</a></li>';
+    htmlNavTemplate += '        {{/types}}';
+    htmlNavTemplate += '    </ul>';
+    htmlNavTemplate += '</div>';
+
     htmlGroupStartTemplate = '' +
         '<div class="publications-anchor">' +
-        '    <a name="publications-{{ id }}"></a>' +
+        '    <a name="publications-{{ type }}"></a>' +
         '</div>' +
         '<div class="panel panel-default">' +
         '    <div class="panel-heading">' +
@@ -337,12 +357,12 @@ var renderPublications = function(jsonData) {
     htmlTemplate = '' +
         '        <div class="publication row">' +
         '            <div class="index col-md-1" id="publication-{{ index }}">{{ index }}</div>' +
-        '            <div class="col-md-2"><a href="{{ paperUrl }}">' +
-        '                <img class="img-thumbnail" src="{{ thumbUrl }}" alt=""/></a>' +
+        '            <div class="col-md-2"><a href="{{ link }}">' +
+        '                <img class="img-thumbnail" src="img/publications/{{ id }}.jpg" alt=""/></a>' +
         '            </div>' +
         '            <div class="col-md-6">' +
         '                <div class="publications-date">{{{ date }}}</div>' +
-        '                {{{ authors }}} <a href="{{ paperUrl }}"><strong>{{{ title }}}</strong></a>. {{{ context }}}' +
+        '                {{{ authors }}} <a href="{{ link }}"><strong>{{{ title }}}</strong></a>. {{{ publication }}}' +
         '                {{#acceptanceRate}}' +
         '                <div class="publications-acceptance">' +
         '                    [Acceptance Rate: {{ acceptanceRate }}]' +
@@ -353,21 +373,9 @@ var renderPublications = function(jsonData) {
         '                {{/awards}}' +
         '            </div>' +
         '            <div class="col-md-3">' +
-        '                {{#paperUrl}}' +
-        '                <a href="{{ paperUrl }}"><div class="badge-publication"><span class="label label-info"><span class="glyphicon glyphicon-file"></span> paper</span></div></a>' +
-        '                {{/paperUrl}}' +
-        '                {{#videoUrl}}' +
-        '                <a href="{{ videoUrl }}"><div class="badge-video"><span class="label label-info"><span class="glyphicon glyphicon-film"></span> video</span></div></a>' +
-        '                {{/videoUrl}}' +
-        '                {{#talkVideoUrl}}' +
-        '                <a href="{{ talkVideoUrl }}"><div class="badge-video"><span class="label label-info"><span class="glyphicon glyphicon-film"></span> talk video</span></div></a>' +
-        '                {{/talkVideoUrl}}' +
-        '                {{#dlUrl}}' +
-        '                <a href="{{ dlUrl }}"><div class="badge-publication"><span class="label label-info"><span class="glyphicon glyphicon-book"></span> Digital Library</span></div></a>' +
-        '                {{/dlUrl}}' +
-        '                {{#project}}' +
-        '                <a href="#{{ project }}"><div class="badge-publication"><span class="label label-info"><span class="glyphicon glyphicon-tag"></span> project info</span></div></a>' +
-        '                {{/project}}' +
+        '                {{#links}}' +
+        '                    <a href="{{ url }}"><div class="badge-publication"><span class="label label-info"><span class="glyphicon glyphicon-{{ icon }}"></span> {{ title }}</span></div></a>' +
+        '                {{/links}}' +
         '            </div>' +
         '        </div>';
 
@@ -376,28 +384,85 @@ var renderPublications = function(jsonData) {
         return;
     }
 
+    // Group publications by publication.type
+    for (var i = 0; i < jsonData.length; i += 1) {
+        publicationGroup = getArrayElementWithAttribute(publicationGroups, {"name":"type", "value":jsonData[i].type});
+        if (publicationGroup) {
+            publicationGroup["rows"].push(jsonData[i]);
+        } else {
+            publicationGroup = {"type":jsonData[i].type, "rows":[jsonData[i]]};
+            publicationGroups.push(publicationGroup);
+        }
+    }
+
+    // Sort types by their sortOrder attribute
+    typesJsonData.sort(function(a, b){
+        if (a.sortOrder > b.sortOrder) {
+            return 1;
+        }
+        if (a.sortOrder < b.sortOrder) {
+            return -1;
+        }
+        return 0;
+    });
+
+    // Sort groups by the sort order defined in types json data
+    publicationGroups.sort(function(a, b){
+        var aType = getArrayElementWithAttribute(typesJsonData, {"name":"name", "value": a.type}),
+            bType = getArrayElementWithAttribute(typesJsonData, {"name":"name", "value": b.type});
+
+        if (!aType) {
+            console.log("No publication type found with name: " + a.type);
+            return 1;
+        }
+        if (!bType) {
+            console.log("No publication type found with name: " + b.type);
+            return -1;
+        }
+
+        if (aType.sortOrder > bType.sortOrder) {
+            return 1;
+        }
+        if (aType.sortOrder > bType.sortOrder) {
+            return -1;
+        }
+        return 0;
+    });
+
+
+    // Render HTML from templates
     renderedHTML = '';
-//    for(groupId = 0; groupId < jsonData.length; groupId++) { // TODO: GRUMMER - Group publications by publication.type
-//        publicationGroup = jsonData[groupId];
-//
-//        publicationGroup["id"] = groupId;
-//
-//        renderedHTML += Mustache.render(htmlGroupStartTemplate, publicationGroup);
-//
-//        for(publicationId = 0; publicationId < publicationGroup["publications"].length; publicationId++) {
-//            publication = publicationGroup["publications"][publicationId];
-//
-//            if (publication.links) {
-//                publication.links = addIconsToLinks(publication.links);
-//            }
-//
-//            renderedHTML += Mustache.render(htmlTemplate, publication);
-//        }
-//
-//        renderedHTML += Mustache.render(htmlGroupEndTemplate, publicationGroup);
-//    }
-//
-//    $parent.append(renderedHTML);
+
+    renderedHTML += Mustache.render(htmlNavTemplate, {"types":typesJsonData});
+
+    for(var groupId = 0; groupId < publicationGroups.length; groupId += 1) {
+        publicationGroup = publicationGroups[groupId];
+        publicationType = getArrayElementWithAttribute(typesJsonData, {"name":"name", "value": publicationGroup.type});
+
+        publicationGroup["id"] = groupId;
+        if (publicationType) {
+            publicationGroup["title"] = publicationType.displayName;
+        } else {
+            console.log("No publication type found with name: " + publicationGroup.type);
+            publicationGroup["title"] = publicationGroup.type;
+        }
+
+        renderedHTML += Mustache.render(htmlGroupStartTemplate, publicationGroup);
+
+        for(publicationId = 0; publicationId < publicationGroup["rows"].length; publicationId++) {
+            publication = publicationGroup["rows"][publicationId];
+
+            if (publication.links) {
+                publication.links = addIconsToLinks(publication.links);
+            }
+
+            renderedHTML += Mustache.render(htmlTemplate, publication);
+        }
+
+        renderedHTML += Mustache.render(htmlGroupEndTemplate, publicationGroup);
+    }
+
+    $parent.append(renderedHTML);
 };
 
 var renderTalks = function(jsonData) {
