d87e315d58aa38636a9fc011dac617b569580805
Gabe Cohn <gabe@microsoft.com>
2015-04-14 19:56:49 -0700

    added events JSON

---
+++ json/events.js
@@ -0,0 +1,39 @@
+/* Events JSON structure
+ *  Each event object contains the following:
+ *      title - string, event title to be displayed
+ *      location - string or NULL, shown in parenthesis if specified
+ *      displayDate - string, date to be displayed
+ *      sortDate - string in the form MM-DD-YYYY. Used to style events as past or future.
+ */
+var eventsJSON = [
+    {
+        "title": "UIST 2014",
+        "location": "Honolulu",
+        "displayDate": "Oct 5-8",
+        "endDate": "10-08-2014"
+    },
+    {
+        "title": "SenSys 2014",
+        "location": "Memphis",
+        "displayDate": "Nov 3-6",
+        "endDate": "11-06-2014"
+    },
+    {
+        "title": "Ubicomp 2015 PC",
+        "location": "NYC",
+        "displayDate": "May 16-17",
+        "endDate": "05-17-2015"
+    },
+    {
+        "title": "Ubicomp 2015",
+        "location": "Osaka",
+        "displayDate": "Sept 7-11",
+        "endDate": "09-11-2015"
+    },
+    {
+        "title": "UIST 2015",
+        "location": "Charlotte",
+        "displayDate": "Nov 8-11",
+        "endDate": "11-11-2015"
+    }
+];
